0f614b3a044e7a896ef5342456435027b65cdc4d
Đỗ Ngọc Tuấn Duy <143875347+dntuanduy-124@users.noreply.github.com>
2024-10-20 08:46:10 +0700

    Update QuanLyQuyenController.cs

--- WebsiteBanHang/Controllers/QuanLyQuyenController.cs
+++ WebsiteBanHang/Controllers/QuanLyQuyenController.cs
@@ -51,7 +51,6 @@ namespace WebsiteBanHang.Controllers
         public ActionResult ChinhSua([Bind(Include = "MaQuyen,TenQuyyen")] Quyen quyen)
         {
             //if (ModelState.IsValid)
-            
             Quyen q =  db.Quyens.Find(quyen.MaQuyen);
             TryUpdateModel(q, new string[] { "MaQuyen", "TenQuyen"});       //ko dc phep doi ma quyen
             db.SaveChanges();
